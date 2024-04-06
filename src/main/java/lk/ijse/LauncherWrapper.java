ddb33befa4c344948c7d874802a69f9a9b0ed1c8
chamithdilhara41 <chamithdilhara41@gmail.com>
2024-04-06 20:04:35 +0530

    dashbord form create but not yet

--- src/main/java/lk/ijse/LauncherWrapper.java
+++ src/main/java/lk/ijse/LauncherWrapper.java
@@ -1,12 +1,6 @@
 package lk.ijse;
 
-import javafx.scene.control.Alert;
-import lk.ijse.Launcher;
-import lk.ijse.db.DbConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 public class LauncherWrapper {
     public static void main(String[] args) {
--- src/main/java/lk/ijse/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/controller/DashboardFormController.java
@@ -1,6 +1,62 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class DashboardFormController {
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    void btnOnActionBuyer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionDashboard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionEmployee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionOrders(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionPayment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionStock(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionSupplier(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionTransactions(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOnActionVehicle(ActionEvent event) {
+
+    }
 
 }
