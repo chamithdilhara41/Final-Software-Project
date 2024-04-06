b2190e69190413392037f6e45435f7cace788ad2
chamithdilhara41 <chamithdilhara41@gmail.com>
2024-04-06 20:13:37 +0530

    dashbord form create but not yet

--- src/main/java/lk/ijse/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/controller/DashboardFormController.java
@@ -2,7 +2,14 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardFormController {
 
@@ -28,8 +35,13 @@ public class DashboardFormController {
     }
 
     @FXML
-    void btnOnActionLogout(ActionEvent event) {
-
+    void btnOnActionLogout(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Login Form");
+        stage.show();
     }
 
     @FXML
