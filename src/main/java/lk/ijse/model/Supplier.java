0214ff184011960f8d059086d467d4d42c68e045
chamithdilhara41 <chamithdilhara41@gmail.com>
2024-04-10 10:19:14 +0530

    add lombok form pom

--- src/main/java/lk/ijse/model/Supplier.java
+++ src/main/java/lk/ijse/model/Supplier.java
@@ -1,5 +1,11 @@
 package lk.ijse.model;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data //getter & setter toString okkoma tiyenawa
+
 public class Supplier {
     private String supplierId;
     private String supplierName;
@@ -7,65 +13,5 @@ public class Supplier {
     private String supplierContact;
     private String supplierGender;
 
-    public Supplier() {
-    }
 
-    public Supplier(String supplierId, String supplierName, String supplierAddress, String supplierContact, String supplierGender) {
-        this.supplierId = supplierId;
-        this.supplierName = supplierName;
-        this.supplierAddress = supplierAddress;
-        this.supplierContact = supplierContact;
-        this.supplierGender = supplierGender;
-    }
-
-    public String getSupplierId() {
-        return supplierId;
-    }
-
-    public void setSupplierId(String supplierId) {
-        this.supplierId = supplierId;
-    }
-
-    public String getSupplierName() {
-        return supplierName;
-    }
-
-    public void setSupplierName(String supplierName) {
-        this.supplierName = supplierName;
-    }
-
-    public String getSupplierAddress() {
-        return supplierAddress;
-    }
-
-    public void setSupplierAddress(String supplierAddress) {
-        this.supplierAddress = supplierAddress;
-    }
-
-    public String getSupplierContact() {
-        return supplierContact;
-    }
-
-    public void setSupplierContact(String supplierContact) {
-        this.supplierContact = supplierContact;
-    }
-
-    public String getSupplierGender() {
-        return supplierGender;
-    }
-
-    public void setSupplierGender(String supplierGender) {
-        this.supplierGender = supplierGender;
-    }
-
-    @Override
-    public String toString() {
-        return "Supplier{" +
-                "supplierId='" + supplierId + '\'' +
-                ", supplierName='" + supplierName + '\'' +
-                ", supplierAddress='" + supplierAddress + '\'' +
-                ", supplierContact='" + supplierContact + '\'' +
-                ", supplierGender='" + supplierGender + '\'' +
-                '}';
-    }
 }
