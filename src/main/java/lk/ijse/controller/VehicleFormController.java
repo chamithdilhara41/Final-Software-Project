5a5e0aaaa2cb710fa3bde4267778b009d300a5bd
chamithdilhara41 <chamithdilhara41@gmail.com>
2024-04-16 13:36:32 +0530

    add vehicale with out logics

--- src/main/java/lk/ijse/controller/VehicleFormController.java
+++ src/main/java/lk/ijse/controller/VehicleFormController.java
@@ -2,10 +2,16 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import lk.ijse.model.Vehicle;
+import lk.ijse.repository.SupplierRepo;
+import lk.ijse.repository.VehicleRepo;
+
+import java.sql.SQLException;
 
 public class VehicleFormController {
 
@@ -31,7 +37,7 @@ public class VehicleFormController {
 
     @FXML
     void btnOnActionClear(ActionEvent event) {
-
+        clearFields();
     }
 
     @FXML
@@ -41,7 +47,22 @@ public class VehicleFormController {
 
     @FXML
     void btnOnActionSave(ActionEvent event) {
+        String vehicleNo = txtVehicleNo.getText();
+        String vehicleType = txtVehicleType.getText();
 
+        Vehicle vehicle = new Vehicle(vehicleNo, vehicleType);
+
+        try {
+            boolean isSaved = VehicleRepo.save(vehicle);
+            if(isSaved) {
+                new Alert(Alert.AlertType.CONFIRMATION, "Vehicle saved!").show();
+                clearFields();
+//                getAllSuppliers();
+//                setCellValueFactory();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
 
     @FXML
@@ -52,5 +73,10 @@ public class VehicleFormController {
     @FXML
     void txtOnActionSearch(ActionEvent event) {
 
+    }
+
+    private void clearFields() {
+        txtVehicleNo.setText("");
+        txtVehicleType.setText("");
     }
 }
--- src/main/java/lk/ijse/repository/VehicleRepo.java
+++ src/main/java/lk/ijse/repository/VehicleRepo.java
@@ -1,4 +1,22 @@
 package lk.ijse.repository;
 
+import lk.ijse.db.DbConnection;
+import lk.ijse.model.Supplier;
+import lk.ijse.model.Vehicle;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class VehicleRepo {
+
+    public static boolean save(Vehicle vehicle) throws SQLException {
+        String sql = "INSERT INTO vehicle VALUES(?, ?);";
+
+        PreparedStatement pstm = DbConnection.getInstance().getConnection().prepareStatement(sql);
+
+        pstm.setObject(1, vehicle.getVehicleNo());
+        pstm.setObject(2, vehicle.getVehicleType());
+
+        return pstm.executeUpdate() > 0;
+    }
 }
