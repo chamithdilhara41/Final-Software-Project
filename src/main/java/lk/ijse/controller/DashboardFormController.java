0a2d1a5cc2b72047865caf3506ce07f0c8962deb
chamithdilhara41 <chamithdilhara41@gmail.com>
2024-04-17 22:59:34 +0530

    done dusted Employee with 1 to many relationship

--- src/main/java/lk/ijse/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/controller/DashboardFormController.java
@@ -1,14 +1,68 @@
 package lk.ijse.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import lk.ijse.db.DbConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DashboardFormController {
+
+    private int supplierCount;
+
+    private int employeeCount;
 
     @FXML
     private Label lblEmployeeCount;
 
     @FXML
     private Label lblSupplierCount;
+
+    public void initialize() {
+        try {
+            supplierCount = getSupplierCount();
+            employeeCount = getEmployeeCount();
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+        setSupplierCount(supplierCount);
+        setEmployeeCount(employeeCount);
+    }
+
+    private void setEmployeeCount(int employeeCount) {
+        lblEmployeeCount.setText(String.valueOf(employeeCount));
+    }
+
+    private int getEmployeeCount() throws SQLException {
+        String sql = "SELECT COUNT(*) AS employee_count FROM employee";
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        ResultSet resultSet = pstm.executeQuery();
+        if(resultSet.next()) {
+            return resultSet.getInt("employee_count");
+        }
+        return 0;
+
+    }
+
+    private void setSupplierCount(int supplierCount) {
+        lblSupplierCount.setText(String.valueOf(supplierCount));
+    }
+
+    private int getSupplierCount() throws SQLException {
+        String sql = "SELECT COUNT(*) AS supplier_count FROM supplier";
+
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        ResultSet resultSet = pstm.executeQuery();
+
+        if(resultSet.next()) {
+            return resultSet.getInt("supplier_count");
+        }
+        return 0;
+    }
 
 }
